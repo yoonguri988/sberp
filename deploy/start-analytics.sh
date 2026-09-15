7ed0a5b1d1ef6313a314ff3fd4731d61ee461d92
yoonguri988 <alice951816@gmail.com>
2026-09-15 16:31:10 +0900

    del: 필요없는 파일 삭제

--- deploy/start-analytics.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/../analytics-django"
-source .venv/bin/activate
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
--- deploy/start-back.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/../back"
-JAR=$(ls build/libs/*.jar | grep -v plain | head -n1)
-exec java -jar "$JAR"
